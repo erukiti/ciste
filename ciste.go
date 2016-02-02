4d8215f72ec9f99e34887ed56e7fab536f4ecf66
erukiti <erukiti@gmail.com>
2016-02-02 15:23:29 +0900

    implement: ciste ssh command

--- ciste.go
+++ ciste.go
@@ -1,87 +1,87 @@
 package main
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 	"os/exec"
+	"strings"
 )
 
-func execShellScript(script string) ([]byte, error) {
-	f, err := ioutil.TempFile(os.TempDir(), "ciste")
-	if err != nil {
-		return nil, err
-	}
-	defer os.Remove(f.Name())
-
-	f.Write([]byte(script))
-
-	return exec.Command("sh", f.Name()).CombinedOutput()
-}
-
-func isInstalled(name string) bool {
-	_, err := exec.LookPath(name)
-	return err == nil
-}
-
-func sha256Sum(data []byte) string {
-	bytes := sha256.Sum256(data)
-	return hex.EncodeToString(bytes[:])
-}
-
-func extractTar(src []byte, dest string) ([]byte, error) {
-	f, err := ioutil.TempFile(os.TempDir(), "ciste")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer os.Remove(f.Name())
-	f.Write(src)
-
-	return execShellScript(fmt.Sprintf("cd %s && tar zxvf %s", dest, f.Name()))
-}
-
-func install(name string, url string, hash string, dest string) error {
-	var err error
-	log.Printf("%s installing... ", name)
-
-	if isInstalled(name) {
-		log.Println("already installed.")
-		return nil
-	}
-
-	res, err := http.Get(url)
-	if err != nil {
-		log.Println("failed.")
-		return err
-	}
-	data, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
-	if err != nil {
-		log.Println("failed.")
-		return err
-	}
-	if sha256Sum(data) != hash {
-		log.Println("failed.")
-		return fmt.Errorf("download file sha256 illegal.")
-	}
-
-	_, err = extractTar(data, dest)
-	if err != nil {
-		log.Println("failed.")
-		return err
-	} else {
-		log.Println("succeeded.")
-		return nil
-	}
-}
-
 func main() {
-	err := install("docker", "https://get.docker.com/builds/Linux/x86_64/docker-1.9.1.tgz", "6a095ccfd095b1283420563bd315263fa40015f1cee265de023efef144c7e52d", "/")
+	var err error
+
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	logWriter, err := os.OpenFile("log.txt", os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("log file error: %s\n", err)
+	} else {
+		log.SetOutput(logWriter)
 	}
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+		cwd = "/home/git"
+	}
+
+	original := os.Getenv("SSH_ORIGINAL_COMMAND")
+	sp := strings.Split(original, " ")
+	originalCommand := sp[0]
+	repo := strings.Trim(sp[1], "'")
+
+	switch os.Args[1] {
+	case "ssh":
+		// len
+
+		repoPath := fmt.Sprintf("%s/repository/%s", cwd, repo)
+
+		_, err := os.Stat(repoPath)
+		if err != nil && os.IsNotExist(err) {
+			os.MkdirAll(repoPath, 0755)
+			cmd := exec.Command("git", "init", "--bare")
+			cmd.Dir = repoPath
+			out, err := cmd.Output()
+			if err != nil {
+				log.Println(string(out))
+				log.Println(err)
+				os.Exit(1)
+			}
+			log.Println(string(out))
+
+			hookPath := fmt.Sprintf("%s/hooks/pre-receive", repoPath)
+
+			code := fmt.Sprintf("#! /bin/sh\ncat | %s receive %s %s %s\n", os.Args[0], os.Args[2], os.Args[3], repo)
+			ioutil.WriteFile(hookPath, []byte(code), 0755)
+		}
+
+		receiveCommand := fmt.Sprintf("%s 'repository/%s'", originalCommand, repo)
+		log.Println(receiveCommand)
+		cmd := exec.Command("git-shell", "-c", receiveCommand)
+		stdin, err := cmd.StdinPipe()
+		defer stdin.Close()
+		stdout, err := cmd.StdoutPipe()
+		defer stdout.Close()
+
+		go io.Copy(stdin, os.Stdin)
+		go io.Copy(os.Stdout, stdout)
+		err = cmd.Run()
+		if err != nil {
+			log.Println(err)
+			os.Exit(1)
+		}
+
+	case "receive":
+		log.Println("receive start")
+
+		fmt.Println("hoge")
+
+	}
+
+	_ = cwd
+
+	os.Exit(1)
 }
